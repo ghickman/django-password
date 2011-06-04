ea6673f199b82e29f03d91123ca9a868964eef44
George Hickman <george@ghickman.co.uk>
2011-06-04 22:09:05 +0100

    Allowed no group to be selected for a password, thus making it private

--- password/models.py
+++ password/models.py
@@ -9,7 +9,7 @@ class Password(models.Model):
     domain = models.CharField(max_length=255, null=True, blank=True)
     passwd = models.CharField(max_length=255)
     user = models.ForeignKey(User, related_name='incunapassword_employee_related')
-    group = models.ManyToManyField(Group, help_text='Deselect all groups to make password private.')
+    group = models.ManyToManyField(Group, blank=True, help_text='Deselect all groups to make password private.')
 
     class Meta:
         ordering = ('name',)
